c27b2980f93d8e5abcb7bde1032585ee55bc8cf8
sangcs321 <21130511@st.hcmuaf.edu.vn>
2025-11-23 10:30:16 +0700

    Revert "refactor package.json"

--- PublicModule.Client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-import "./App.scss";
-import { RouterProvider } from "react-router";
-import { AuthRoute } from "routes";
-
-export const App = () => {
-  return <RouterProvider router={AuthRoute} />;
-};
--- PublicModule.Client/src/layouts/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface AuthLayoutProps {
-  children: React.ReactNode;
-}
-
-export const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <div>{children}</div>;
-};
