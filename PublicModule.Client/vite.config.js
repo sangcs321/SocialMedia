7a15657c109441cda7e0ab42090dea9c56d04a76
Thanh Sang <thanhsangdang34@gmail.com>
2025-12-01 20:05:33 +0700

    optimize run project

--- PublicModule.Client/vite.config.js
+++ PublicModule.Client/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from "vite-plugin-mkcert";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert(), tsConfigPaths()],
+  server: {
+    proxy: {
+      "^/(api|login|users/me|refresh)": {
+        target: "https://localhost:5229", // ASP.NET Core HTTPS port
+        secure: false, // dev self-signed
+        changeOrigin: true,
+      },
+    },
+    port: 5173,
+    https: {},
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      lodash: "lodash-es",
+    },
+  },
+  optimizeDeps: {
+    include: ["lodash-es"],
+  },
 })
