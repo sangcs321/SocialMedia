f4dbd3b44205f9aee928f1d4006a5b57d2a9c27b
Thanh Sang <thanhsangdang34@gmail.com>
2025-12-03 19:31:41 +0700

    build home page

---
+++ PublicModule.Client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import "./HomePage.module.scss";
+
+const HomePage = () => {
+  return (
+    <div className="app">
+      <div className="appBody">HomePage</div>
+    </div>
+  );
+};
+
+export default HomePage;
